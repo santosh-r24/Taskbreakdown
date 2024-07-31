0145aa52fb3055e38c917c16b653710524f2241f
Santosh <santoshramakrishnan24@gmail.com>
2024-07-31 22:49:17 +0530

    adding helper/utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -1,4 +1,5 @@
 """Contains utility functions"""
+import random
 import streamlit as st
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
@@ -8,6 +9,7 @@ from logzero import logger
 import helper.database_functions as db_funcs
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
+
 def initialize_variables():
     """
     initializes streamlit variables used in the session state, and loads user messages.
@@ -23,9 +25,17 @@ def initialize_variables():
     st.session_state['chat_model'] = None
     st.session_state['plan_model'] = None
     st.session_state['plan'] = None
+    st.session_state['rate_limit'] = st.secrets['message_rate_limit']
+    st.session_state['timeframe'] = st.secrets['timeframe_in_mins']
+    initialize_api_key()
 
     if not st.session_state['display_messages']:
         st.session_state['display_messages'] = cached_get_user_chat_messages(st.session_state['user_info']['email'], None)
+
+def initialize_api_key():
+    if 'gemini_api_key' not in st.session_state:
+        st.session_state['gemini_api_key'] = random.choice(list(st.secrets['api_keys'].values()))
+        logger.debug(f"This session uses the key {st.session_state['gemini_api_key']}")
 
 @st.cache_data(show_spinner=False)
 def cached_get_user_chat_messages(email: str, timestamp=None):
@@ -36,6 +46,12 @@ def cached_get_user_chat_messages(email: str, timestamp=None):
 def cached_get_latest_summary(email: str):
     db, cursor = db_funcs.initialize_database()
     return db_funcs.get_latest_summary(cursor, email)
+
+@st.cache_data(ttl=300)  # Cache for 5 minutes
+def cached_get_message_count(email, timeframe):
+    """Cache function to fetch message count"""
+    db, cursor = db_funcs.initialize_database()
+    return db_funcs.get_message_count_within_timeframe(cursor, email, timeframe)
 
 def check_if_user_and_api_keys_are_set():
     if not st.session_state['user_info'] and st.session_state['gemini_api_key']:
@@ -49,23 +65,26 @@ def initialise_ui_layout_todolist_page():
     """
     Sets the initial UI display of Todolist tab elements.
     """
-    st.header("To-Do List Agent")
+    st.header("To-Do List Smart Assistant")
     st.markdown(''' :blue-background[Tip: Read the instructions to get a better understanding on how the tool can help you!] ''')
     with st.popover("Instructions"):
         st.markdown('''
-                    1. The Agent follows the [SMART framework](https://www.atlassian.com/blog/productivity/how-to-write-smart-goals) to help define goals.
+                    1. The assistant follows the [SMART framework](https://www.atlassian.com/blog/productivity/how-to-write-smart-goals) to help define goals.
                     2. Being specific and giving supporting details will help to curate a personalised plan.
-                    3. If you're unsure of the details, send a general goal, the agent will help you asking details for context.
-                    4. Set Timeline dates, and schedule time to help create a more accurate timeline. 
-                    5. Timelines **must** be set in order to generate plans, and to sync to calendar. 
-                    6. The agent generates a summary of older messages every so often (after every 5000 Tokens), and preserves newer messages for precise context.
+                    3. If you're unsure of the details, send a general goal, the assistant will help you asking details for context.
+                    4. Set :blue-background[Timeline dates] for accurate start and end date of goals.
+                    5. Set :blue-background[schedule time] to help the assistant understand the amount of time you can spend on your preparation days. 
+                    6. The agent generates a summary of older messages every so often (after every 5000 Tokens, or ~3500 words including assitant's response), and preserves newer messages for precise context.
                     7. You can see if a summary has been generated, by toggling the summary button.
-                    8. If a summary hasn't been generated, the agent uses all previous messages as context. 
+                    8. If a summary hasn't been generated, the assistant uses all previous messages as context. 
                     9. Currently the summary can't be edited.
-                    10. Currently, the agent doesn't store plans generated, or synced to calendar. 
-                    11. The agent only *remembers* the latest summary (if present) and the messages post it.
-                    12. :blue-background[Delete chat] in the side bar can be used to delete all chat interactions with the agent.
-                    13. :blue-background[Delete summary] in the side bar, deletes all summaries.
+                    10. Timelines **must** be set in order to generate plans, and to sync to calendar. 
+                    11. Once the assistant gives you a plan, you can head to the Calendar plan tab to view plans, and sync to your calendar. 
+                    11. Currently, the assistant doesn't store plans generated in :blue-background[Calendar plan] tab, or synced to google calendar. 
+                    12. The assistant only *remembers* the latest summary (if present) and the messages generated after it.
+                    13. :blue-background[Delete chat] in the side bar can be used to delete all chat interactions with the assistant, and clear the context of the assistant.
+                    14. :blue-background[Delete summary] in the side bar, deletes all summaries.
+                    15. If you want to create a new unrelated plan, delete chat and start afresh.
                     ''')
     st.divider()
 
@@ -90,7 +109,7 @@ def delete_summary_records(cursor, connection):
     Function to clear summary maintained by the model. This function is called when the Delete summary button is clicked.
     """
     st.warning("All summaries associated will be deleted!")
-    st.write("Type '**delete summary **' to proceed")
+    st.write("Type '**delete summary**' to proceed")
     delete_check = st.text_input("Enter delete key")
     if st.button("Submit") and delete_check == "delete summary":
         db_funcs.delete_summaries(cursor, connection, st.session_state['user_info']['email'])
